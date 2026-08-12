5cc9b37aa7fe33836c80de2f512d1a06d2e7608a
Jenkins <jenkins@experian.com>
2026-08-12 11:46:36 +0000

    Release 8.0.9 - Updated XCFramework URL and checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeviceInsightCollector",
-            url: "https://github.com/udi-collectors/deviceinsight-collector-ios/releases/download/8.1.0-0-SNAPSHOT/deviceinsight-collector-ios-xcframework-8.1.0-0-SNAPSHOT.zip",
+            url: "https://github.com/udi-collectors/deviceinsight-collector-ios/releases/download/8.0.9/deviceinsight-collector-ios-xcframework-8.0.9.zip",
             checksum: "1af16d7e2027c6980d7e88a34f7e6157fa7f77013d0d865704038bef1cfdf415"
         )
     ]
